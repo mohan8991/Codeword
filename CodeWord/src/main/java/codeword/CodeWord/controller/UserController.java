3ba2a760a85b30feb218aa1bbb34f0d51ab72dc7
Mohan Krishna G K <mohan.8991@gmail.com>
2019-04-07 19:58:13 -0400

    Application started return JSON for user '/user'

--- CodeWord/src/main/java/codeword/CodeWord/controller/UserController.java
+++ CodeWord/src/main/java/codeword/CodeWord/controller/UserController.java
@@ -1,5 +1,17 @@
 package codeword.CodeWord.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import codeword.CodeWord.UserEntity.User;
+
+@RestController
 public class UserController {
+	
+	@RequestMapping("/user")
+	public User getUser(@RequestParam(value="name", defaultValue="world") String Name) {
+		return new User();
+	}
 
 }
